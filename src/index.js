e9d1ed6cc3da0b27a1dbeabbafd7c9843df4e599
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-13 22:11:38 +0500

    errorlarni yasab oldim.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// import error handler
+const errorhandler = require("./shared/errors/handle");
+
 // import router
 const address_route = require("./modules/product/_api");
 
 // registered router
 app.use(address_route);
+
+// registered error handler
+app.use(errorhandler);
 
 app.listen(5001, () => {
   console.log(`SERVER HAS BEEN STARTED ON LOCAL PORT ${5001}`);
